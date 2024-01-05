644c65909663035fbebb6417d5d647e7be957a3c
Tomislav <tomislav.zivko@fronttribe.com>
2024-01-05 20:27:52 +0100

    feat: card component

--- src/components/VehicleCard/index.js
+++ src/components/VehicleCard/index.js
@@ -1,9 +1,14 @@
-import { Card, Text, Image, Paper } from "@mantine/core";
+import { Card, Text, Image, Badge, Group } from "@mantine/core";
 import React from "react";
 import { vehicleCardStyle } from "../../styles/VehicleCard/styles";
 import { useHover } from "@mantine/hooks";
 
-export const VehicleCard = () => {
+export const VehicleCard = ({
+  data,
+  renderButtons,
+  modelDescription,
+  onClick,
+}) => {
   const { hovered, ref } = useHover();
 
   return (
@@ -16,19 +21,51 @@ export const VehicleCard = () => {
         withBorder
         styles={hovered ? vehicleCardStyle.hovered : vehicleCardStyle}
       >
-        <Card.Section>
+        <Card.Section
+          p={10}
+          onClick={onClick}
+          styles={{ section: { cursor: "pointer" } }}
+        >
           <Text fw={600} mt={10} align="center">
-            BMW
+            {data.name}
           </Text>
 
           <Image
-            p={10}
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+            p={5}
+            src={data.image}
             height={160}
             w="100%"
             alt="Norway"
+            onClick={onClick}
           />
+          <Text fw={400} mt={5} align="start">
+            {data.abrv}
+          </Text>
+          <Text fw={400} mt={5} align="start">
+            Manufacturer:{" "}
+            <Badge bg="black" c="white">
+              {data.manufacturer}
+            </Badge>
+          </Text>
         </Card.Section>
+        {modelDescription && (
+          <Card.Section mt="xs" p="md">
+            <Text
+              fz="sm"
+              fw={500}
+              c="gray.7"
+              mb="sm"
+              tt="uppercase"
+              style={{ lineHeight: 1 }}
+            >
+              Model description:
+            </Text>
+
+            <Group>{modelDescription}</Group>
+          </Card.Section>
+        )}
+
+        <Card.Section p="md">{renderButtons}</Card.Section>
       </Card>
     </div>
   );
