acc06368a8e211489e4a5d9234f5e26e232e10a2
Tomislav <tomislav.zivko@fronttribe.com>
2024-01-05 20:26:13 +0100

    feat: sort button

--- src/components/SortBtn/index.js
+++ src/components/SortBtn/index.js
@@ -1,11 +1,12 @@
 import { Select } from "@mantine/core";
 import React from "react";
 
-export const SortBtn = () => {
+export const SortBtn = ({ data, onChange, initialValue = "" }) => {
   return (
     <Select
-      placeholder="Sort by"
-      data={["React", "Angular", "Vue", "Svelte"]}
+      data={data}
+      value={initialValue}
+      onChange={(value) => onChange(value)}
       clearable
     />
   );
